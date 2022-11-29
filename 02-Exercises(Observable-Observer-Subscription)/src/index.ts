083d621a7c5960cbe752deaca4cb57dde54a158d
João Teixeira <joao.teixeira@mb-c4qgcn7w2m.speedport.ip>
2022-11-29 23:06:51 +0100

    exercises

--- 02-Exercises(Observable-Observer-Subscription)/src/index.ts
+++ 02-Exercises(Observable-Observer-Subscription)/src/index.ts
@@ -1,3 +1,18 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
+const observable$ = new Observable<string>((subscriber) => {
+    subscriber.next("Alice");
+    subscriber.next("Ben");
+    setTimeout(() => {
+        subscriber.next("Charlize");
+        subscriber.complete();
+    }, 2000);
+    return () => {
+        console.log("Teardown");
+    };
+});
 
+observable$.subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log("Completed"),
+});
