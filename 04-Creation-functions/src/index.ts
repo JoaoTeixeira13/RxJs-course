6d9e4f3d9e01e3808a0b25f2700e95b93dcd50de
João Teixeira <j.teixeira.dev@gmail.com>
2022-12-01 12:07:12 +0100

    fromEvent function

--- 04-Creation-functions/src/index.ts
+++ 04-Creation-functions/src/index.ts
@@ -1,4 +1,4 @@
-import { Observable, of, from } from "rxjs";
+import { Observable, of, from, fromEvent } from "rxjs";
 
 //of
 
@@ -54,3 +54,40 @@ obervableFromPromise$.subscribe({
     error: (err) => console.log("Error: ", err),
     complete: () => console.log("Promise completed"),
 });
+
+//fromEvent
+
+const triggerButton = document.querySelector("button#trigger");
+
+const subscription = fromEvent<MouseEvent>(triggerButton, "click").subscribe(
+    (event) => console.log(event.type, event.x, event.y)
+);
+
+setTimeout(() => {
+    console.log("Unsubscribe");
+    subscription.unsubscribe();
+}, 5000);
+
+const triggerClick$ = new Observable<MouseEvent>((subscriber) => {
+    const clickHandlerFn = (event: MouseEvent) => {
+        console.log("Event callback executed");
+        subscriber.next(event);
+    };
+    triggerButton.addEventListener("click", clickHandlerFn);
+
+    
+    // teardown logic, prevent memory leaks (without this the event call back keeps executing)
+
+    return () => {
+        triggerButton.removeEventListener("click", clickHandlerFn);
+    };
+});
+
+const subscription2 = triggerClick$.subscribe((event) =>
+    console.log("from triggerClick$ observable", event.type, event.x, event.y)
+);
+
+setTimeout(() => {
+    console.log("Unsubscribe");
+    subscription2.unsubscribe();
+}, 5000);
