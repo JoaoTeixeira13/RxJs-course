c6574fc7a3dab34ddd7443d1f8ba004eed3de027
João Teixeira <j.teixeira.dev@gmail.com>
2022-12-04 15:34:58 +0100

    BehaviourSubject

--- 06-Subjects/src/index.ts
+++ 06-Subjects/src/index.ts
@@ -1,4 +1,11 @@
-import { fromEvent, map, Subject } from "rxjs";
+import {
+    BehaviorSubject,
+    ConnectableObservable,
+    fromEvent,
+    map,
+    Subject,
+    withLatestFrom,
+} from "rxjs";
 
 const emitButton = document.querySelector("button#emit");
 const inputElement: HTMLInputElement = document.querySelector("#value-input");
@@ -14,3 +21,33 @@ fromEvent(subscribeButton, "click").subscribe(() => {
     console.log("New Subscription");
     value$.subscribe((value) => console.log(value));
 });
+
+const loggedInSpan: HTMLElement = document.querySelector("span#logged-in");
+const loginButton: HTMLElement = document.querySelector("button#login");
+const logoutButton: HTMLElement = document.querySelector("button#logout");
+const printStateButton: HTMLElement =
+    document.querySelector("button#print-state");
+
+const isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+fromEvent(loginButton, "click").subscribe(() => isLoggedIn$.next(true));
+fromEvent(logoutButton, "click").subscribe(() => isLoggedIn$.next(false));
+
+//navigation bar
+
+isLoggedIn$.subscribe(
+    (isLoggedIn) => (loggedInSpan.innerText = isLoggedIn.toString())
+);
+
+//buttons
+
+isLoggedIn$.subscribe((isLoggedIn) => {
+    logoutButton.style.display = isLoggedIn ? "block" : "none";
+    loginButton.style.display = !isLoggedIn ? "block" : "none";
+});
+
+fromEvent(printStateButton, "click")
+    .pipe(withLatestFrom(isLoggedIn$))
+    .subscribe(([event, isLoggedIn]) =>
+        console.log("User is logged in:", isLoggedIn)
+    );
